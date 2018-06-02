8accedeea053a324ba61b4c9fe660033a75ea8fe
Sameer Khan <sameer_khan@student.uml.edu>
2018-06-02 16:32:32 -0400

    Added functionality for numbers and operations

--- Main.js
+++ Main.js
@@ -3,8 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { pressNum } from './reducer';
+import { enter, pressNum, operation } from './reducer';
 import Button from './Button';
+
+const baseNumber = {
+  backgroundColor: '#424242',
+  textAlign: 'right',
+  padding: 10,
+  fontSize: 30,
+  fontWeight: 'bold',
+  borderBottomWidth: 1,
+  borderColor: '#fff',
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,15 +26,17 @@ const styles = StyleSheet.create({
   bottom: {
     flex: 1,
   },
-  number: {
+  append: {
     color: '#fff',
-    backgroundColor: '#424242',
-    textAlign: 'right',
-    padding: 10,
-    fontSize: 30,
-    fontWeight: 'bold',
-    borderBottomWidth: 1,
-    borderColor: '#fff',
+    ...baseNumber,
+  },
+  replace: {
+    color: '#2e71e5',
+    ...baseNumber,
+  },
+  push: {
+    color: '#9bc23c',
+    ...baseNumber,
   },
   row: {
     flex: 1,
@@ -32,50 +44,58 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = ({ currentNumber, pressNumWithDispatch }) => (
+const App = ({
+  calculatorState: { stack, inputState },
+  pressNumWithDispatch,
+  enterAction,
+  operationAction,
+}) => (
   <View style={styles.container}>
     <View style={styles.top}>
-      <Text style={styles.number}>0</Text>
-      <Text style={styles.number}>0</Text>
-      <Text style={styles.number}>{currentNumber}</Text>
+      <Text style={styles.append}>{stack[2] || 0}</Text>
+      <Text style={styles.append}>{stack[1] || 0}</Text>
+      <Text style={styles[inputState]}>{stack[0] || 0}</Text>
     </View>
     <View style={styles.bottom}>
       <View style={styles.row}>
         <Button text="clear" />
-        <Button text="pow" />
+        <Button text="pow" onPress={operationAction} />
         <Button text="swap" />
-        <Button text="/" />
+        <Button text="/" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="9" onPress={pressNumWithDispatch} />
         <Button text="8" onPress={pressNumWithDispatch} />
         <Button text="7" onPress={pressNumWithDispatch} />
-        <Button text="*" />
+        <Button text="*" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="6" onPress={pressNumWithDispatch} />
         <Button text="5" onPress={pressNumWithDispatch} />
         <Button text="4" onPress={pressNumWithDispatch} />
-        <Button text="-" />
+        <Button text="-" onPress={operationAction} />
       </View>
       <View style={styles.row}>
         <Button text="3" onPress={pressNumWithDispatch} />
         <Button text="2" onPress={pressNumWithDispatch} />
         <Button text="1" onPress={pressNumWithDispatch} />
-        <Button text="+" />
+        <Button text="+" onPress={operationAction} />
       </View>
       <View style={styles.row}>
-        <Button text="0" />
-        <Button text="." />
-        <Button text="enter" special />
+        <Button text="0" onPress={pressNumWithDispatch} />
+        <Button text="." onPress={pressNumWithDispatch} />
+        <Button text="enter" onPress={enterAction} special />
       </View>
     </View>
   </View>
 );
 
-const mapStateToProps = state => ({ currentNumber: state });
-const mapDispatchToProps = dispatch => bindActionCreators({
-  pressNumWithDispatch: pressNum,
-}, dispatch);
+const mapStateToProps = state => ({ calculatorState: state });
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({
+    pressNumWithDispatch: pressNum,
+    enterAction: enter,
+    operationAction: operation,
+  }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- reducer.js
+++ reducer.js
@@ -1,11 +1,54 @@
 const PRESS_NUM = 'PRESS_NUM';
+const ENTER = 'ENTER';
+const OPERATION = 'OPERATION';
 
 export const pressNum = n => ({ type: PRESS_NUM, payload: n });
+export const enter = () => ({
+  type: ENTER,
+});
+export const operation = op => ({
+  type: OPERATION,
+  payload: op,
+});
 
-export const reducer = (state = '0', { type, payload }) => {
+// inputState can be append | replace | push
+const doOperation = (x, y, op) => {
+  if (op === 'pow') return y ** x;
+  else if (op === '-') return y - x;
+  else if (op === '+') return y + x;
+  else if (op === '*') return y * x;
+};
+
+export const reducer = (state = { stack: [], inputState: 'replace' }, { type, payload }) => {
   switch (type) {
+    case OPERATION:
+      return {
+        stack: [`${doOperation(parseFloat(state.stack[0]), parseFloat(state.stack[1]), payload)}`, ...state.stack.slice(2)],
+        inputState: 'push',
+      };
+    case ENTER:
+      return {
+        stack: [state.stack[0] || '0', ...state.stack],
+        inputState: 'replace',
+      };
     case PRESS_NUM:
-      return state + payload;
+      if (state.inputState === 'replace') {
+        return {
+          stack: [payload, ...state.stack.slice(1)],
+          inputState: 'append',
+        };
+      } else if (state.inputState === 'append') {
+        return {
+          stack: [(state.stack[0] || '0') + payload, ...state.stack.slice(1)],
+          inputState: 'append',
+        };
+      } else if (state.inputState === 'push') {
+        return {
+          stack: [payload, ...state.stack],
+          inputState: 'append',
+        };
+      }
+      break;
     default:
       return state;
   }
